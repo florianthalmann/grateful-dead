6cad4e64209595d453dfa777da3ca8da8e6af2bb
Florian Thalmann <flo@lt91-49.eecs.qmul.ac.uk>
2016-01-22 17:54:42 +0000

    added some images

--- app/scripts/controllers/maincontroller.js
+++ app/scripts/controllers/maincontroller.js
@@ -4,7 +4,7 @@
 	angular.module('gratefulDead.controllers')
 		.controller('MainController', ['$scope', '$http', function($scope, $http){
 			
-			$scope.songs = [{name:"Dark Star", versions:[{name:"studio"}, {name:"live1"}, {name:"live2"}]}, {name:"Sugar Magnolia", versions:[{name:"studio"}, {name:"live1"}, {name:"live2"}]}, {name:"Fire on the Mountain", versions:[{name:"studio"}, {name:"live1"}, {name:"live2"}]}];
+			$scope.songs = [{name:"Dark Star", versions:[{name:"studio", image:"http://www.psilo.com/dead/images/tickets/tweir991120.jpg"}, {name:"live1", image:"http://www.psilo.com/dead/images/tickets/t810313.jpg"}, {name:"live2", image:"http://www.psilo.com/dead/images/tickets/t820313.jpg"}]}, {name:"Sugar Magnolia", versions:[{name:"studio", image:"http://www.psilo.com/dead/images/tickets/t820313.jpg"}, {name:"live1", image:"http://www.psilo.com/dead/images/tickets/t810313.jpg"}, {name:"live2", image:"http://www.psilo.com/dead/images/tickets/tweir991120.jpg"}]}, {name:"Fire on the Mountain", versions:[{name:"studio", image:"http://www.psilo.com/dead/images/tickets/tweir991120.jpg"}, {name:"live1", image:"http://www.psilo.com/dead/images/tickets/t820313.jpg"}, {name:"live2", image:"http://www.psilo.com/dead/images/tickets/t810313.jpg"}]}];
 			
 			$scope.selectedSong;
 			
