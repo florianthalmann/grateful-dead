75f41e98bc7d82194500202c40fb3baf07f43ee6
Florian Thalmann <florianthalmann@bluewin.ch>
2016-07-06 17:12:11 +0200

    added dependency of specific dymo-core version for now

--- server.js
+++ server.js
@@ -9,8 +9,8 @@
 	app.use(express["static"](__dirname + '/app'));
 	app.use(bodyParser.json({limit: '50mb'}));
 	
-	app.listen("8090");
+	app.listen("8088");
 	
-	console.log('Server started at http://localhost:8090');
+	console.log('Server started at http://localhost:8088');
 	
 }).call(this);
